3d61e48f63fcba907cc137e7d701a7d9b8aeeafe
Kate Orlova <kate.orlova@deleteagency.com>
2020-01-06 14:25:42 +0500

    Added a notification to the console on the operation completion

--- CRUD/create-item.ps1
+++ CRUD/create-item.ps1
@@ -1,1 +1,2 @@
 New-Item -Path "master:\sitecore\content\home\MyNewItems" -Name "MyNewItem" -ItemType "\sitecore\templates\User Defined\Project\My Page types\Standard content page"
+Write-Host "Finished"
