9fd0d743bc3a43751b2277157ca59c67930d3976
Kate Orlova <kate.orlova@deleteagency.com>
2020-01-10 19:23:40 +0500

    Delete many bucket items

--- CRUD/delete-many-bucket-items.ps1
+++ CRUD/delete-many-bucket-items.ps1
@@ -3,6 +3,7 @@ $targetPath = "master:\sitecore\content\home\MyBucketItems"
 $bulk = New-Object "Sitecore.Data.BulkUpdateContext"
 try {
 	$buckets = Get-ChildItem -Path $targetPath -Recurse
+	Write-Host "Deleteing $($buckets.Length) item(s) from '$($targetPath)'"
 	$buckets | Remove-Item
 }
 finally {
