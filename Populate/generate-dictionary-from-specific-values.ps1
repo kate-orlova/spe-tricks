e907d11c2e8c3566a4de9b590c52d2f1c6ad60bc
Kate Orlova <kate.orlova@deleteagency.com>
2020-02-01 18:05:19 +0500

    Generate a dictionary from some values

--- Populate/generate-dictionary-from-specific-values.ps1
+++ Populate/generate-dictionary-from-specific-values.ps1
@@ -8,3 +8,7 @@ $colours = @(
 $systemDictionaryPath = "master:/sitecore/system/Dictionary"
 $systemDictionaryFolderTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary folder"
 $systemDictionaryEntryTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary entry"
+
+$coloursDictionary = New-Item -Path $systemDictionaryPath -Name "Colours" -type $systemDictionaryFolderTemplate
+
+Write-Host "Finished"
