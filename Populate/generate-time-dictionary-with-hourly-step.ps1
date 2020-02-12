e7fca38dcd65610bb12fd39d51e9996854284699
Kate Orlova <kate.orlova@deleteagency.com>
2020-02-12 18:34:14 +0500

    Generate a time dictionary with hourly step

---
+++ Populate/generate-time-dictionary-with-hourly-step.ps1
@@ -0,0 +1,5 @@
+$systemDictionaryPath = "master:/sitecore/system/Dictionary"
+$systemDictionaryFolderTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary folder"
+$systemDictionaryEntryTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary entry"
+
+Write-Host "Finished"
