1d0e9b4f2b00f377302afb841d2b4fdb0ba54e81
Kate Orlova <kate.orlova@deleteagency.com>
2020-02-12 18:36:23 +0500

    Generate a time dictionary with hourly step

--- Populate/generate-time-dictionary-with-hourly-step.ps1
+++ Populate/generate-time-dictionary-with-hourly-step.ps1
@@ -2,4 +2,6 @@ $systemDictionaryPath = "master:/sitecore/system/Dictionary"
 $systemDictionaryFolderTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary folder"
 $systemDictionaryEntryTemplate = "master:/sitecore/templates/System/Dictionary/Dictionary entry"
 
+$hoursDictionary = New-Item -Path $systemDictionaryPath -Name "Hours" -type $systemDictionaryFolderTemplate
+
 Write-Host "Finished"
