b29a4bc0d04b9487d1b0967daad4614e52db9c3d
Kate Orlova <kate.orlova@deleteagency.com>
2020-03-01 11:35:59 +0500

    Update rendering value

--- CRUD/update-rendering-value.ps1
+++ CRUD/update-rendering-value.ps1
@@ -7,5 +7,8 @@ $items = Get-ChildItem -Path $sourcePath -Recurse | Where-Object { $_.TemplateNa
  
 if ($items)
 {
-
+    Write-Host "'$($items.Length)' to be processed"
+    $counter = 0
+	
+	Write-Host "Finished"
 }
