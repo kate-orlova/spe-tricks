c5ae1b4dffaa8a5376ce0e39e1ffec24b69eb85c
Kate Orlova <kate.orlova@deleteagency.com>
2020-04-14 22:31:22 +0500

    Convert a droplist value to a droplink reference

--- CRUD/convert-droplist-value-to-droplink-reference.ps1
+++ CRUD/convert-droplist-value-to-droplink-reference.ps1
@@ -1,4 +1,5 @@
 $products = Get-ChildItem -Path "master:\sitecore\content\home\Products" -Recurse  | Where-Object { $_.TemplateName -eq "Product" }
+$productTypes = Get-ChildItem -Path "master:\sitecore\content\home\ProductTypes" -Recurse  | Where-Object { $_.TemplateName -eq "ProductType" }
 if ($products)
 {
 	Write-Host "Finished"
