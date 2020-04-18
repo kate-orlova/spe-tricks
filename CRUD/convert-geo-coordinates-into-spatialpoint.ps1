cd4eadf369e5e5f7397c98e5acab002165069e35
Kate Orlova <kate.orlova@deleteagency.com>
2020-04-18 18:42:45 +0500

    Convert GEO coordinates into SpatialPoint

--- CRUD/convert-geo-coordinates-into-spatialpoint.ps1
+++ CRUD/convert-geo-coordinates-into-spatialpoint.ps1
@@ -1,3 +1,6 @@
 using namespace Sitecore.ContentSearch.Spatial.DataTypes
 
 $places = Get-ChildItem -Path "master:\sitecore\content\home\Places" -Recurse  | Where-Object { $_.TemplateName -eq "Place" }
+if ($places)
+{
+}
