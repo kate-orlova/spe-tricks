83f8ae194f2029cf10b632c126be894013dea7d8
Kate Orlova <kate.orlova@deleteagency.com>
2020-12-14 22:26:33 +0500

    Update an item field value

--- CRUD/update-item-field-value.ps1
+++ CRUD/update-item-field-value.ps1
@@ -1,5 +1,6 @@
 $sourcePath = "master:\sitecore\content\home\MyItems"
 $itemTemplate = "Service"
+$newFieldValue = "New field value"
 
 $items = Get-ChildItem -Path $sourcePath -Recurse | Where-Object { $_.TemplateName -eq $itemTemplate }
  
